9d63f9df779c38276aeaa99c2c87516c80a08f45
Fredrik Lundkvist <fredrik.1994.lundkvist@gmail.com>
2022-05-10 13:34:24 +0200

    påbörjade med kundvagnen

--- frontend/src/Views/Cart/CartView.jsx
+++ frontend/src/Views/Cart/CartView.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import '../Cart/CartView.css'
 
 const CartView = () => {
   return (
-    <div>CartView</div>
+    <div className='Cart'>
+      <div className="container">
+          <h2 className='header'>Kundvagn</h2>
+            <div className='form-cart'>
+              <div className="cart-item">
+                <img className='img-1' src='https://www.ikea.com/se/sv/images/products/taernaby-bordslampa-beige__1016210_pe830263_s5.jpg?f=l' />
+
+                    <div className="text-group">
+                      <h1>Växjö</h1>
+                      <small>299 :-</small>
+                    </div>
+
+                    <div className='button-group'>
+                      <button>-</button>
+                      <button>+</button>
+                    </div>
+              </div>
+              <div className="cart-item">
+                <img className='img-1' src='https://www.ikea.com/se/sv/images/products/taernaby-bordslampa-beige__1016210_pe830263_s5.jpg?f=l' />
+
+                    <div className="text-group">
+                      <h1>Växjö</h1>
+                      <small>299 :-</small>
+                    </div>
+
+                    <div className='button-group'>
+                      <button>-</button>
+                      <button>+</button>
+                    </div>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 

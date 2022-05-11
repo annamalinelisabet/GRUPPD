fe3767527e06142aeb725d8db40fddd053a0dca2
Fredrik Lundkvist <fredrik.1994.lundkvist@gmail.com>
2022-05-11 14:51:46 +0200

    Klar med cart-item

--- frontend/src/Views/Cart/CartView.jsx
+++ frontend/src/Views/Cart/CartView.jsx
@@ -9,21 +9,24 @@ const CartView = () => {
           <h2 className='header'>Kundvagn</h2>
             <div className='form-cart'>
             <div className="cart-item">
-                <img className='img-1' src='https://www.ikea.com/se/sv/images/products/taernaby-bordslampa-beige__1016210_pe830263_s5.jpg?f=l' />
-                  <div className="text-group">
-                    <h1>Tärnaby</h1>
-                    <p>299 :-</p>
-                  </div>
+                <div className="text-group">
+                  <img className='img-1' alt='img-1' src='https://www.ikea.com/se/sv/images/products/taernaby-bordslampa-beige__1016210_pe830263_s5.jpg?f=l' />
+                    <div className='head-quant'>
+                      <div className='header-p'>
+                        <h1>Tärnaby</h1>
+                        <p>299 :-</p>
+                      </div>
+                        <div className="quantity">
+                          <button className="minus-btn" type="button" name="button"> <small className='minus-icon'>-</small> </button>
+                            <input type="text" name="name" value="1"></input>
+                          <button className="plus-btn" type="button" name="button"> <small className='plus-icon'>+</small> </button>
+                        </div>
+                    </div>
+                </div>
 
-                  <div class="quantity">
-                    <button className="minus-btn" type="button" name="button"> <small className='minus-icon'>-</small> </button>
-                      <input type="text" name="name" value="1"></input>
-                    <button className="plus-btn" type="button" name="button"> <small className='plus-icon'>+</small> </button>
-                  </div>
-
-                  <div className=''>
+                  <div className='info-group'>
                     <button className='remove-icon'>X</button>
-                    <div class="item-price">349 :-</div>
+                    <div className="item-price">349 :-</div>
                   </div>
               </div>
 

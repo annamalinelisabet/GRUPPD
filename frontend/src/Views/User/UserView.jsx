7800876a33ef5d78067030da8a325062d51e170b
Malin Andersson <annamalinelisabet@gmail.com>
2022-05-24 11:07:30 +0200

    DONE!

--- frontend/src/Views/User/UserView.jsx
+++ frontend/src/Views/User/UserView.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { logoutUser } from '../../store/actions/authActions'
 import { Link, useParams } from 'react-router-dom'
 import { getOrdersFromUser } from '../../store/actions/orderActions'
+import { clearCart } from '../../store/actions/cartActions'
 
 const UserView = () => {
 
@@ -13,17 +14,23 @@ const UserView = () => {
   const { data } = useSelector(state => state.order)
   const completed = data.filter(order => order.completed)
   const notCompleted = data.filter(order => !order.completed)
+  
 
 
   useEffect(() => {
     dispatch(getOrdersFromUser(userId))
   }, [dispatch, userId])
 
+  const handleClick = () => {
+    dispatch(logoutUser())
+    dispatch(clearCart())
+  }
+
   return (
     <div className='User'>
         <div className="wrapper">          
             <h2 className='header-text'>MINA SIDOR</h2>
-            <Link onClick={() => dispatch(logoutUser())} to="/login"><button className='btn btn-danger'>LOGGA UT</button></Link>
+            <Link onClick={handleClick} to="/login"><button className='btn btn-danger'>LOGGA UT</button></Link>
             <div>
               <h4 className='order-text'>PÅGÅENDE ORDRAR</h4>
               { notCompleted.length ? notCompleted.map(order => <OrderCard key={order._id} order={order} />)
